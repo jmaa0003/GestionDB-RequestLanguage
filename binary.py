31ac441d24b5490714e9df0af63c9750655c746a
jmaa0003 <119824296+jmaa0003@users.noreply.github.com>
2025-02-24 12:33:17 +0100

    Add files via upload
    1 test reuissi plus que write et continuer

--- binary.py
+++ binary.py
@@ -12,7 +12,7 @@ class BinaryFile:
     def goto(self, pos: int) -> None:
         """Déplace l’endroit pointé dans le fichier à pos bytes après le début du fichier\
             si pos est positif et à pos bytes avant la fin du fichier si est négatif"""
-        if isinstance(pos, int) and self.file.seekable:
+        if isinstance(pos, int) and self.file.seekable():
             if pos >= 0:
                 self.file.seek(pos)
             else:
@@ -35,7 +35,7 @@ class BinaryFile:
     def write_integer_to(self, n: int, size: int, pos: int)-> int:
         """Écrit l’entier n sur size bytes à la pos-ième position dans le fichier\
             ( *voir def goto() )""" 
-        self.file.goto(pos)
+        self.goto(pos)
         if self.file.writable():
             self.file.write(n.to_bytes(length = size, byteorder = 'little', signed = True ))
         return size
@@ -43,20 +43,20 @@ class BinaryFile:
 
     def write_string(self, s: str)-> int:
         """écrit la chaîne de caractère s à l’endroit pointé actuellement par le fichier"""
-        previous_file_size = self.file.get_size()
+        previous_file_size = self.get_size()
         if self.file.writable():
-            self.file.write(s.to_bytes(byteorder = 'little', signed = True ))
-        return self.file.get_size() - previous_file_size
+            self.file.write(s.encode())
+        return self.get_size() - previous_file_size
         
 
     def write_string_to(self, s: str, pos: int) -> int:
         """écrit la chaîne de caractère s à la pos-ième* position dans le fichier\
             ( *voir def goto() )"""
-        previous_file_size = self.file.get_size()
-        self.file.goto(pos)
+        previous_file_size = self.get_size()
+        self.goto(pos)
         if self.file.writable():
-            self.file.write(s.to_bytes(byteorder = 'little', signed = True ))
-        return self.file.get_size() - previous_file_size
+            self.file.write(s.encode())
+        return self.get_size() - previous_file_size
     
 
     def read_integer(self, size: int)-> int:
@@ -64,16 +64,16 @@ class BinaryFile:
             actuellement par le fichier"""
         if self.file.readable():
             encoded_integer = self.file.read(size)
-        return int.from_bytes(encoded_integer, length = size, byteorder = 'little', signed = True )
+        return int.from_bytes(encoded_integer, byteorder = 'little', signed = True )
 
     def read_integer_from(self, size: int, pos: int)-> int:
         """renvoie l’entier encodé sur size bytes à partir de la pos-ième*\
             position dans le fichier\
         ( *voir def goto() )"""
-        self.file.goto(pos)
+        self.goto(pos)
         if self.file.readable():
             encoded_integer = self.file.read(size)
-        return int.from_bytes(encoded_integer, length = size, byteorder = 'little', signed = True )
+        return int.from_bytes(encoded_integer, byteorder = 'little', signed = True )
         
 
     def read_string(self)-> str:
@@ -88,7 +88,7 @@ class BinaryFile:
         """renvoie la chaîne de caractères encodée à partir de la pos-ième*\
             position dans le fichier\
         ( *voir def goto() )"""
-        self.file.goto(pos)
+        self.goto(pos)
         if self.file.readable():
             encoded_string = self.file.read()
         return bytes.decode(encoded_string[::-1])
