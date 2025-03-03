8e3c0ead1bef7bab63b0c5d3415ca30c2a8af07f
jmaa0003 <119824296+jmaa0003@users.noreply.github.com>
2025-03-03 16:03:59 +0100

    Add files via upload

--- database.py
+++ database.py
@@ -1,33 +1,45 @@
 from binary import BinaryFile
 from enum import Enum
+import os
 
 class FieldType(Enum):
-    INTEGER, STRING = (1).to_bytes(), (2).to_bytes()
+    INTEGER, STRING = 1, 2
 
 
 class Database:
+    """Dans cette classe, toutes les méthodes renvoient ValueError si l'opération liée est impossible"""
     TableSignature = list[tuple[str, FieldType]]
     
     def __init__(self, name: str):
         self.name = name
-        
+
 
     def create_table(self, table_name: str, *fields: TableSignature) -> None:
-        """crée une nouvelle table de nom table_name et de signature fields"""
+        """crée une nouvelle table de nom table_name et de signature fields."""
+        if os.path.exists(f"{table_name}.table"):
+            raise ValueError
+        else:
+            with open(f"{table_name}.table", "x") as tb:
+                pass #TODO écrire avec header ...
+            
 
 
     def list_tables(self) -> list[str]:
-        """ renvoie une liste avec le nom de toutes les tables existant dans cette DB"""
-        offset_tb_signature, returned_name, returned_list = 9, "", []
-        offset_length_then_name = self.BinaryFile.read_integer_from(2, offset_tb_signature) + 1
-        for i in range(offset_length_then_name):
-            returned_name += self.BinaryFile.read_string()
-        
-        #TODO: faire les fonctions dans l'ordre des test + boucle de renvoie noms list_tables
-        
+        """renvoie une liste avec le nom de toutes les tables existant dans cette DB"""
+        list_of_names = []
+        for pseudo_table in os.listdir():
+            if pseudo_table.endswith(".table"):
+                list_of_names.append(pseudo_table.rsplit(".table")[0])
+        return list_of_names
+    
     
     def delete_table(self, table_name: str) -> None:
-        """supprime la table de nom table_name. Renvoie VlaueError Si l’opération est impossible """
-       
+        """supprime la table de nom table_name."""  
+        pass
+
 
     def get_table_signature(self, table_name: str) -> TableSignature:
+        pass
+
+
+    
