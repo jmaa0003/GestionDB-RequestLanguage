9525aef03a53136dc070b69809bd067cf5833ade
jmaa0003 <119824296+jmaa0003@users.noreply.github.com>
2025-03-05 16:34:38 +0100

    Add files via upload

--- database.py
+++ database.py
@@ -17,11 +17,28 @@ class Database:
     def create_table(self, table_name: str, *fields: TableSignature) -> None:
         """crée une nouvelle table de nom table_name et de signature fields."""
         if os.path.exists(f"{table_name}.table"):
-            raise ValueError
+            raise ValueError(f'{table_name}.table already stands in this path')
         else:
-            with open(f"{table_name}.table", "x") as tb:
-                pass #TODO écrire avec header ...
-            
+            with open(f"{table_name}.table", "wb+") as tb:
+                table_file, initial_buffer_size = BinaryFile(tb), 16
+                # SIGNATURE
+                table_file.write_string('ULDB')
+                table_file.write_integer(len(fields), len(fields))
+                for one_field, a_field_type in fields:
+                    on_x_bytes = len(one_field)
+                    table_file.write_integer(a_field_type.value, FieldType.INTEGER.value)
+                    table_file.write_integer(on_x_bytes, 2)
+                    table_file.write_string(one_field)
+
+                offset_string_buffer = tb.tell() + 12
+                for i in range(2):
+                    table_file.write_integer(offset_string_buffer, 4) #première place dans le string buffer écrite aussi
+                for i in range(2):
+                    table_file.write_integer(0, 4)
+                for i in range(2):
+                    table_file.write_integer(-1, 4)
+                # STRING BUFFER
+                table_file.write_integer(0, initial_buffer_size)
 
 
     def list_tables(self) -> list[str]:
@@ -34,12 +51,16 @@ class Database:
     
     
     def delete_table(self, table_name: str) -> None:
-        """supprime la table de nom table_name."""  
-        pass
-
+        """supprime la table de nom table_name.""" 
+        try:
+            os.remove(f"{table_name}.table")
+        except FileNotFoundError:
+            raise ValueError(f"{table_name}.table does not stand in this path.")
+        
 
     def get_table_signature(self, table_name: str) -> TableSignature:
-        pass
-
-
-    
+        try:
+            with open(f"{table_name}.table", "rb+") as tb:
+                pass #TODO
+        except FileNotFoundError:
+            raise ValueError(f"{table_name}.table does not stand in this path.")
